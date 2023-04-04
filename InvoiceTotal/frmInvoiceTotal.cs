76e2537818583deb55381895b61220dc7acfd504
Harrison <47501747+Code-acus@users.noreply.github.com>
2023-04-03 19:31:04 -0600

    Update frmInvoiceTotal.cs

--- InvoiceTotal/frmInvoiceTotal.cs
+++ InvoiceTotal/frmInvoiceTotal.cs
@@ -19,18 +19,41 @@ namespace InvoiceTotal
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
         }
 
+        //private void btnCalculate_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        decimal subtotal = Decimal.Parse(txtSubtotal.Text);
+        //        decimal discountPercent = 0.1m; // You can change this value as needed or fetch it from another source.
+        //        decimal discountAmount = subtotal * discountPercent;
+        //        decimal total = subtotal - discountAmount;
+
+        //        txtDiscountPercent.Text = (discountPercent * 100).ToString("0.00") + "%";
+        //        txtDiscountAmount.Text = discountAmount.ToString("c");
+        //        txtTotal.Text = total.ToString("c");
+        //    }
+        //    catch (FormatException)
+        //    {
+        //        MessageBox.Show("Please enter a valid number for the subtotal.", "Input Error");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show("An unexpected error has occurred: " + ex.Message, "Error");
+        //    }
+        //}
+
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             try
             {
-                decimal subtotal = Convert.ToDecimal(txtSubtotal.Text);
+                decimal subtotal = Decimal.Parse(txtSubtotal.Text);
                 decimal discountPercent = 0.1m; // You can change this value as needed or fetch it from another source.
-                decimal discountAmount = subtotal * discountPercent;
-                decimal total = subtotal - discountAmount;
+                decimal discountAmount = Math.Round(subtotal * discountPercent, 2);
+                decimal total = Math.Round(subtotal - discountAmount, 2);
 
                 txtDiscountPercent.Text = (discountPercent * 100).ToString("0.00") + "%";
-                txtDiscountAmount.Text = discountAmount.ToString("c");
-                txtTotal.Text = total.ToString("c");
+                txtDiscountAmount.Text = discountAmount.ToString(); // Removed formatting code
+                txtTotal.Text = total.ToString(); // Removed formatting code
             }
             catch (FormatException)
             {
@@ -41,6 +64,8 @@ namespace InvoiceTotal
                 MessageBox.Show("An unexpected error has occurred: " + ex.Message, "Error");
             }
         }
+
+
 
         private void btnExit_Click(object sender, EventArgs e)
         {
